7a809ac2f23230508bca19086a33428906313aa8
raul.mulerschat <raul.mulerschat@movtech.com.br>
2024-02-26 18:36:05 -0300

    feature(general): adiciona funções de save e restore

--- src/app/pages/pages-configurations/pages-configurations.component.ts
+++ src/app/pages/pages-configurations/pages-configurations.component.ts
@@ -25,16 +25,6 @@ export class PagesConfigurationsComponent implements OnInit {
     this.borderRGBA = this.hexToRGBA(this.bordeBar, 0.5)
   }
 
-  saveConfigMain() {
-    localStorage.setItem('bar', this.bar);
-    localStorage.setItem('bordeBar', this.bordeBar);
-
-  }
-
-  saveConfigLinear(){
-    localStorage.setItem('barLinear', this.bar);
-    localStorage.setItem('bordeBarLienar', this.bordeBar);
-  }
   hexToRGBA(hexValue: string, opacity: number): string {
     const hex = hexValue.replace('#', '');
     const rgbValues = hex.match(/.{1,2}/g)?.map(val => parseInt(val, 16)) ?? [];
@@ -42,4 +32,94 @@ export class PagesConfigurationsComponent implements OnInit {
     return `rgba(${rgbValues.join(',')})`;
   }
 
+  saveConfigMain(){}
+  saveConfigChartMain(){
+    localStorage.setItem('MainBar', this.bar);
+    localStorage.setItem('MainBordeBar', this.bordeBar);
+  }
+
+  restoreConfigChartMain(){
+    localStorage.setItem('MainBar', this.bar);
+    localStorage.setItem('MainBordeBar', this.bordeBar);
+  }
+
+
+
+  saveConfigPowerTrendLine() {
+    localStorage.setItem('TrendLinePowerColor', this.bar);
+    localStorage.setItem('TrendLinePowerWidth', this.bordeBar);
+
+  }   
+  restoreConfigPowerTrendLine() {
+    localStorage.setItem('TrendLinePowerColor', this.bar);
+    localStorage.setItem('TrendLinePowerWidth', this.bordeBar);
+
+  } 
+  
+  
+
+  saveConfigLogarithmicTrendLine(){
+    localStorage.setItem('LogarithmicTrendLineColor', this.bar);
+    localStorage.setItem('LogarithmicTrendLineWidth', this.bordeBar);
+  }
+
+  restoreConfigLogarithmicTrendLine(){
+    localStorage.setItem('LogarithmicTrendLineColor', this.bar);
+    localStorage.setItem('LogarithmicTrendLineWidth', this.bordeBar);
+  }
+
+
+
+
+
+
+
+  saveConfigExponentialTrendLine(){
+    localStorage.setItem('ExponentialTrendLineColor', this.bar);
+    localStorage.setItem('ExponentialTrendLineWidth', this.bordeBar);
+  }
+  restoreConfigExponentialTrendLine(){
+    localStorage.setItem('ExponentialTrendLineColor', this.bar);
+    localStorage.setItem('ExponentialTrendLineWidth', this.bordeBar);
+  }
+ 
+
+
+
+
+  saveConfigCorrelationTable(){
+    localStorage.setItem('CorrelationTableMinimum', this.bar);
+    localStorage.setItem('CorrelationTableMaximum', this.bordeBar);
+    localStorage.setItem('CorrelationTableFilter', this.bordeBar);
+    localStorage.setItem('CorrelationTableFilterValueMinimum', this.bordeBar);
+    localStorage.setItem('CorrelationTableFilterValueMaximum', this.bordeBar);
+  }
+  restoreConfigCorrelationTable(){
+    localStorage.setItem('CorrelationTableMinimum', this.bar);
+    localStorage.setItem('CorrelationTableMaximum', this.bordeBar);
+    localStorage.setItem('CorrelationTableFilter', this.bordeBar);
+    localStorage.setItem('CorrelationTableFilterValueMinimum', this.bordeBar);
+    localStorage.setItem('CorrelationTableFilterValueMaximum', this.bordeBar);
+  }
+ 
+
+  saveConfigOutlier(){
+    localStorage.setItem('OutlierDeviantMaximumValue', this.bar);    
+  }
+  restoreConfigOutlier(){
+    localStorage.setItem('OutlierDeviantMaximumValue', this.bar);    
+  }
+ 
+ 
+
+  saveConfigMeanAritmethic(){
+    localStorage.setItem('MeanAritmethicColor', this.bar);
+    localStorage.setItem('MeanAritmethicWidth', this.bordeBar);
+  }
+  restoreConfigMeanAritmethic(){
+    localStorage.setItem('MeanAritmethicColor', this.bar);
+    localStorage.setItem('MeanAritmethicWidth', this.bordeBar);
+  }
+
+ 
 }
