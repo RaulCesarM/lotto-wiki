c595ed95499f8b28f04a96fd2fb53d49d29e61dd
Raul Cesar Mulerschat <raulzito737@gmail.com>
2024-03-20 21:58:51 -0300

    .

--- src/app/services/katex.service.ts
+++ src/app/services/katex.service.ts
@@ -23,30 +23,19 @@ export class KatexService {
   }
 
   getExponentialTrendLineFormula(): string {
-    return `
-   {y = ae^{bx}},
-    \\quad b = \\frac{{\\ln\\left(\\frac{{\\text{{data}}[\\text{{data.length}} - 1]}}{{a}}\\right)}}{{\\text{{data.length}} - 1}},
-    \\quad\\text{{tl-exp}}[i] = a \\cdot e^{b \\cdot i}
-    `;
+    return `{y = a\\cdot e^{bx}},`;
   }
 
   getArithmeticTrendLineFormula(): string {
-    return `
-    y = mx + b,
-    \\quad m = \\frac{{n \\sum xy - \\sum x \\sum y}}{{n \\sum x^2 - (\\sum x)^2}},
-    \\quad b = \\frac{{\\sum y - m \\sum x}}{{n}},
-    \\quad \\text{{tl-arith}}[i] = m (i + 1) + b
-    `;
+    return ` y = m\\cdot x + b`;
   }
 
-  getLogarithmicTrendLineFormula(): string {
-    return `
-    y = A \ln(x) + B,
-    \\quad A = \\frac{{n \\sum (\\ln(x) \\cdot y) - \\sum(\\ln(x)) \\cdot \\sum y}}{{n \\sum (\\ln(x))^2 - (\\sum(\\ln(x)))^2}},
-    \\quad B = \\frac{{\\sum y - A \\sum \\ln(x)}}{{n}},
-    \\quad \\text{{tl-log}}[i] = A \ln(i + 1) + B
-    `;
+   getLogarithmicTrendLineFormula() {
+    return` y = A\\cdot \\ln \\cdot(x) + B`;
+
+
 }
+
 
 
 
